0d6840b80e4d8d7d11b2b666ec106d0474c57ab3
Wilson Nascimento de Freitas <wilson.freitas@gmail.com>
2013-09-01 11:57:58 -0300

    setup.py and LICENSE added

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="bizdays",
+      version="0.1.0",
+      py_modules=['bizdays'],
+      author='Wilson Freitas',
+      author_email='wilson.freitas@gmail.com',
+      description='A quick and dirty implementation for business days calculations.',
+      url='https://github.com/wilsonfreitas/bizdays',
+      license='MIT',
+      long_description='''\
+In several countries and markets, to account the value of financial
+instruments, mainly bonds and derivatives, it is necessary the use of different
+rules to compute the way the days go by. Those rules are related to holidays
+which are intrinsically related to counties and local markets. Usually you have
+a list with the holidays and all you want is to find out the number of business
+days between two dates, nothing more. It is necessary for pricing properly the
+financial instrument. bizdays was implemented to do that, compute business days
+between two dates and other collateral effects like adjust a given date for the
+next or previous business day, and so forth. bizdays is a module without
+dependencies, what makes it appropriate for simple implementations.
+''', )
+
