1ae284dd897fb1006d81ce093cec3c59fda1bfc3
wilsonfreitas <wilson.freitas@gmail.com>
2014-10-03 08:26:55 -0300

    Replaced tabs with spaces

--- bizdays.py
+++ bizdays.py
@@ -3,17 +3,17 @@ The calendar specification is a .cal file containing the weekdays to be
 considered as non-business days and a iso-formated list of dates representing
 holidays. It follows an example::
 
-	Saturday
-	Sunday
-	2001-01-01
-	2002-01-01
-	2013-01-01
+    Saturday
+    Sunday
+    2001-01-01
+    2002-01-01
+    2013-01-01
 
 Classes:
-	Calendar
-	CalendarSpec
+    Calendar
+    CalendarSpec
 Functions:
-	create_date_index
+    create_date_index
 '''
 
 import os
@@ -21,196 +21,196 @@ from datetime import datetime, date, timedelta
 import re
 
 def create_date_index(holidays, startdate, enddate, weekdays):
-	""" This function returns the date index.
-	The date index accumulates the amount of business days.
-	This can be used to speed up the amount of business days between 2 dates.
-	Once each date is index the amount of business days between 2 dates is
-	fairly obtained by the subtraction of the respective index value for 
-	each date."""
-	_index = {}
-	d1 = timedelta(1)
-	dt = startdate
-	w = c = 1
-	while dt <= enddate:
-		is_hol = dt in holidays or dt.weekday() in weekdays
-		_index[dt] = (w, c, is_hol)
-		c += 1
-		if not is_hol:
-			w += 1
-		dt += d1
-	return _index
+    """ This function returns the date index.
+    The date index accumulates the amount of business days.
+    This can be used to speed up the amount of business days between 2 dates.
+    Once each date is index the amount of business days between 2 dates is
+    fairly obtained by the subtraction of the respective index value for 
+    each date."""
+    _index = {}
+    d1 = timedelta(1)
+    dt = startdate
+    w = c = 1
+    while dt <= enddate:
+        is_hol = dt in holidays or dt.weekday() in weekdays
+        _index[dt] = (w, c, is_hol)
+        c += 1
+        if not is_hol:
+            w += 1
+        dt += d1
+    return _index
 
 
 class Calendar(object):
-	''' Calendar class to compute business days accordingly a list of holidays.
-	'''
-	_weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
-		 'Saturday', 'Sunday')
-	def __init__(self, holidays=None, startdate=None, enddate=None, weekdays=('Saturday', 'Sunday')):
-		self._holidays = holidays or []
-		self._nonwork_weekdays = [self._weekdays.index(wd) for wd in weekdays]
-		if startdate:
-			self._startdate = datetime.strptime(startdate, '%Y-%m-%d').date()
-		else:
-			self._startdate = date(self._holidays[0].year, 1, 1)
-		if enddate:
-			self._enddate = datetime.strptime(enddate, '%Y-%m-%d').date()
-		else:
-			self._enddate = date(self._holidays[-1].year, 12, 31)
-		
-		self._index = create_date_index(self._holidays, self._startdate, 
-			self._enddate, self._nonwork_weekdays)
-	
-	def __get_weekdays(self):
-		return tuple( self._weekdays[nwd] for nwd in self._nonwork_weekdays )
-	weekdays = property(__get_weekdays)
-	
-	def __get_startdate(self):
-		return self._startdate
-	startdate = property(__get_startdate)
-	
-	def __get_enddate(self):
-		return self._enddate
-	enddate = property(__get_enddate)
-	
-	def __get_holidays(self):
-		return self._holidays
-	holidays = property(__get_holidays)
-	
-	def __get_index(self):
-		return self._index
-	index = property(__get_index)
-	
-	def bizdays(self, dates):
-		'''Returns the number of business days between 2 dates. 
-		dates is a tuple with 2 strings ISO-formated dates (%%Y-%%m-%%d).'''
-		d1, d2 = dates
-		d1 = datetime.strptime(d1, '%Y-%m-%d').date()
-		d1 = self.__adjust_next(d1)
-		d2 = datetime.strptime(d2, '%Y-%m-%d').date()
-		d2 = self.__adjust_previous(d2)
-		if d1 > d2:
-			raise ValueError("The first date must be before the second.")
-		return self._index[d2][0] - self._index[d1][0]
-	
-	def currentdays(self, dates):
-		'''Returns the number of current days between 2 dates.
-		It simply subtracts dates and return the amount of days in timedelta.
-		dates is a tuple with 2 strings ISO-formated dates (%%Y-%%m-%%d).
-		'''
-		d1, d2 = dates
-		d1 = datetime.strptime(d1, '%Y-%m-%d').date()
-		d2 = datetime.strptime(d2, '%Y-%m-%d').date()
-		if d1 > d2:
-			raise ValueError("The first date must be before the second.")
-		return (d2 - d1).days
-	
-	def isbizday(self, dt):
-		'''Returns True whether dt is a business day or False otherwise.
-		dt is a string ISO-formated date (%%Y-%%m-%%d).'''
-		dt = datetime.strptime(dt, '%Y-%m-%d').date()
-		return not self._index[dt][2]
-	
-	def __adjust_next(self, dt):
-		d1 = timedelta(1)
-		while self._index[dt][2]:
-			dt += d1
-		return dt
-		
-	def adjust_next(self, dt):
-		"""Returns the next business day whether the passed date isn't a
-		business day or returns the given date.
-		dt is a string ISO-formated date (%%Y-%%m-%%d).
-		"""
-		dt = datetime.strptime(dt, '%Y-%m-%d').date()
-		return self.__adjust_next(dt).isoformat()
-	
-	def __adjust_previous(self, dt):
-		d1 = timedelta(1)
-		while self._index[dt][2]:
-			dt -= d1
-		return dt
-	
-	def adjust_previous(self, dt):
-		"""Returns the first business day before the passed date whether
-		the given date isn't a business day or returns the given date.
-		dt is a string ISO-formated date (%%Y-%%m-%%d).
-		"""
-		dt = datetime.strptime(dt, '%Y-%m-%d').date()
-		return self.__adjust_previous(dt).isoformat()
-	
-	def seq(self, dates):
-		'''Returns a sequence generator which generates business days between
-		the 2 given dates.
-		dates is a tuple with 2 strings ISO-formated dates (%%Y-%%m-%%d).
-		'''
-		_from, _to = dates
-		d1 = timedelta(1)
-		_from = datetime.strptime(_from, '%Y-%m-%d').date()
-		_to = datetime.strptime(_to, '%Y-%m-%d').date()
-		if _from > _to:
-			raise ValueError("The first date must be before the second.")
-		_from = self.__adjust_next(_from)
-		while _from <= _to:
-			yield _from.isoformat()
-			_from = self.__adjust_next(_from + d1)
+    ''' Calendar class to compute business days accordingly a list of holidays.
+    '''
+    _weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+         'Saturday', 'Sunday')
+    def __init__(self, holidays=None, startdate=None, enddate=None, weekdays=('Saturday', 'Sunday')):
+        self._holidays = holidays or []
+        self._nonwork_weekdays = [self._weekdays.index(wd) for wd in weekdays]
+        if startdate:
+            self._startdate = datetime.strptime(startdate, '%Y-%m-%d').date()
+        else:
+            self._startdate = date(self._holidays[0].year, 1, 1)
+        if enddate:
+            self._enddate = datetime.strptime(enddate, '%Y-%m-%d').date()
+        else:
+            self._enddate = date(self._holidays[-1].year, 12, 31)
+        
+        self._index = create_date_index(self._holidays, self._startdate, 
+            self._enddate, self._nonwork_weekdays)
+    
+    def __get_weekdays(self):
+        return tuple( self._weekdays[nwd] for nwd in self._nonwork_weekdays )
+    weekdays = property(__get_weekdays)
+    
+    def __get_startdate(self):
+        return self._startdate
+    startdate = property(__get_startdate)
+    
+    def __get_enddate(self):
+        return self._enddate
+    enddate = property(__get_enddate)
+    
+    def __get_holidays(self):
+        return self._holidays
+    holidays = property(__get_holidays)
+    
+    def __get_index(self):
+        return self._index
+    index = property(__get_index)
+    
+    def bizdays(self, dates):
+        '''Returns the number of business days between 2 dates. 
+        dates is a tuple with 2 strings ISO-formated dates (%%Y-%%m-%%d).'''
+        d1, d2 = dates
+        d1 = datetime.strptime(d1, '%Y-%m-%d').date()
+        d1 = self.__adjust_next(d1)
+        d2 = datetime.strptime(d2, '%Y-%m-%d').date()
+        d2 = self.__adjust_previous(d2)
+        if d1 > d2:
+            raise ValueError("The first date must be before the second.")
+        return self._index[d2][0] - self._index[d1][0]
+    
+    def currentdays(self, dates):
+        '''Returns the number of current days between 2 dates.
+        It simply subtracts dates and return the amount of days in timedelta.
+        dates is a tuple with 2 strings ISO-formated dates (%%Y-%%m-%%d).
+        '''
+        d1, d2 = dates
+        d1 = datetime.strptime(d1, '%Y-%m-%d').date()
+        d2 = datetime.strptime(d2, '%Y-%m-%d').date()
+        if d1 > d2:
+            raise ValueError("The first date must be before the second.")
+        return (d2 - d1).days
+    
+    def isbizday(self, dt):
+        '''Returns True whether dt is a business day or False otherwise.
+        dt is a string ISO-formated date (%%Y-%%m-%%d).'''
+        dt = datetime.strptime(dt, '%Y-%m-%d').date()
+        return not self._index[dt][2]
+    
+    def __adjust_next(self, dt):
+        d1 = timedelta(1)
+        while self._index[dt][2]:
+            dt += d1
+        return dt
+        
+    def adjust_next(self, dt):
+        """Returns the next business day whether the passed date isn't a
+        business day or returns the given date.
+        dt is a string ISO-formated date (%%Y-%%m-%%d).
+        """
+        dt = datetime.strptime(dt, '%Y-%m-%d').date()
+        return self.__adjust_next(dt).isoformat()
+    
+    def __adjust_previous(self, dt):
+        d1 = timedelta(1)
+        while self._index[dt][2]:
+            dt -= d1
+        return dt
+    
+    def adjust_previous(self, dt):
+        """Returns the first business day before the passed date whether
+        the given date isn't a business day or returns the given date.
+        dt is a string ISO-formated date (%%Y-%%m-%%d).
+        """
+        dt = datetime.strptime(dt, '%Y-%m-%d').date()
+        return self.__adjust_previous(dt).isoformat()
+    
+    def seq(self, dates):
+        '''Returns a sequence generator which generates business days between
+        the 2 given dates.
+        dates is a tuple with 2 strings ISO-formated dates (%%Y-%%m-%%d).
+        '''
+        _from, _to = dates
+        d1 = timedelta(1)
+        _from = datetime.strptime(_from, '%Y-%m-%d').date()
+        _to = datetime.strptime(_to, '%Y-%m-%d').date()
+        if _from > _to:
+            raise ValueError("The first date must be before the second.")
+        _from = self.__adjust_next(_from)
+        while _from <= _to:
+            yield _from.isoformat()
+            _from = self.__adjust_next(_from + d1)
 
-	def offset(self, dt, n):	
-		"""
-		Offsets the given date by n days
-		"""
-		if n >= 0:
-			d1 = timedelta(1)
-			adjust = lambda d: self.__adjust_next(d)
-			dt = adjust(datetime.strptime(dt, '%Y-%m-%d').date())
-		else:
-			d1 = timedelta(-1)
-			adjust = lambda d: self.__adjust_previous(d)
-			dt = adjust(datetime.strptime(dt, '%Y-%m-%d').date())
-			n = abs(n)
-		i = 0
-		while i < n:
-			dt += d1
-			dt = adjust(dt)
-			i += 1
-		return dt.isoformat()
+    def offset(self, dt, n):    
+        """
+        Offsets the given date by n days
+        """
+        if n >= 0:
+            d1 = timedelta(1)
+            adjust = lambda d: self.__adjust_next(d)
+            dt = adjust(datetime.strptime(dt, '%Y-%m-%d').date())
+        else:
+            d1 = timedelta(-1)
+            adjust = lambda d: self.__adjust_previous(d)
+            dt = adjust(datetime.strptime(dt, '%Y-%m-%d').date())
+            n = abs(n)
+        i = 0
+        while i < n:
+            dt += d1
+            dt = adjust(dt)
+            i += 1
+        return dt.isoformat()
 
 class CalendarSpec(Calendar):
-	''' CalendarSpec class to compute business days accordingly a given
-	calendar's specification. The specification has the weekdays which aren't
-	business days (usually known as weekend) and a list of holidays
-	(or non-business days).
-	'''
-	def __init__(self, cal, startdate=None, enddate=None):
-		fname = cal + '.cal'
-		if not os.path.exists(fname):
-			raise Exception('Invalid calendar specification: \
-			file not found (%s)' % fname)
-		self._cal_spec = cal
-		fcal = open(fname)
-		w = '|'.join(self._weekdays)
-		_holidays = []
-		_nonwork_weekdays = []
-		for cal_reg in fcal:
-			cal_reg = cal_reg.strip()
-			if cal_reg is '': continue
-			m = re.match('^%s$' % w, cal_reg)
-			if m:
-				_nonwork_weekdays.append(cal_reg)
-				# _nonwork_weekdays.append(self._weekdays.index(cal_reg))
-			else:
-				dt = datetime.strptime(cal_reg, '%Y-%m-%d').date()
-				_holidays.append(dt)
-		fcal.close()
-		super(CalendarSpec, self).__init__(_holidays, startdate, enddate, _nonwork_weekdays)
-	
-	def __eq__(self, other):
-		return self.startdate == other.startdate and \
-			self.enddate == other.enddate and \
-			self._cal_spec == other._cal_spec
-	
-	def __get_name(self):
-		return self._cal_spec
-	name = property(__get_name)
-	
+    ''' CalendarSpec class to compute business days accordingly a given
+    calendar's specification. The specification has the weekdays which aren't
+    business days (usually known as weekend) and a list of holidays
+    (or non-business days).
+    '''
+    def __init__(self, cal, startdate=None, enddate=None):
+        fname = cal + '.cal'
+        if not os.path.exists(fname):
+            raise Exception('Invalid calendar specification: \
+            file not found (%s)' % fname)
+        self._cal_spec = cal
+        fcal = open(fname)
+        w = '|'.join(self._weekdays)
+        _holidays = []
+        _nonwork_weekdays = []
+        for cal_reg in fcal:
+            cal_reg = cal_reg.strip()
+            if cal_reg is '': continue
+            m = re.match('^%s$' % w, cal_reg)
+            if m:
+                _nonwork_weekdays.append(cal_reg)
+                # _nonwork_weekdays.append(self._weekdays.index(cal_reg))
+            else:
+                dt = datetime.strptime(cal_reg, '%Y-%m-%d').date()
+                _holidays.append(dt)
+        fcal.close()
+        super(CalendarSpec, self).__init__(_holidays, startdate, enddate, _nonwork_weekdays)
+    
+    def __eq__(self, other):
+        return self.startdate == other.startdate and \
+            self.enddate == other.enddate and \
+            self._cal_spec == other._cal_spec
+    
+    def __get_name(self):
+        return self._cal_spec
+    name = property(__get_name)
+    
 
--- test_bizdays.py
+++ test_bizdays.py
@@ -5,114 +5,114 @@ from datetime import date
 from bizdays import *
 
 class TestCalendar(unittest.TestCase):
-	def testCalendar_instanciate(self):
-		'it should instanciate the calendar'
-		cal = Calendar(startdate='2013-01-01', enddate='2013-12-31', weekdays=('Saturday', 'Sunday'))
-		self.assertEqual(cal.startdate.isoformat(), '2013-01-01')
-		self.assertEqual(cal.enddate.isoformat(), '2013-12-31')
-		self.assertEqual(cal.weekdays, ('Saturday', 'Sunday'))
-	
-	def testCalendar_bizdays(self):
-		'it should return the amount of business days'
-		cal = Calendar(startdate='2013-01-01', enddate='2013-12-31')
-		self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-02')), 1)
-		self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-05')), 3)
-		self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-06')), 3)
-		self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-07')), 4)
-	
-	def testCalendar_consistency_check(self):
-		'it should compare bizdays and currentdays'
-		cal = Calendar(startdate='2013-01-01', enddate='2013-12-31', weekdays=[])
-		self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-06')), cal.currentdays(('2013-01-01', '2013-01-06')))
-	
-	def testCalendarSpec_name(self):
-		"it should check the calendar's name"
-		cal = CalendarSpec('Test')
-		self.assertEqual(cal.name, 'Test')
-	
-	def testCalendarSpec(self):
-		'calendar instanciation'
-		with self.assertRaises(Exception):
-			CalendarSpec('AAA')
-		with self.assertRaises(Exception):
-			CalendarSpec('calAAA')
-		cal = CalendarSpec('Test')
-		self.assertEqual(cal, CalendarSpec('Test'))
-		self.assertEqual(cal.startdate.isoformat(), '2001-01-01')
-		self.assertEqual(cal.enddate.isoformat(), '2013-12-31')
-		self.assertEqual(len(cal.holidays), 4)
-		self.assertEqual(date(2001, 1, 1) in cal.holidays, True)
-		self.assertEqual(cal.index[cal.startdate], (1, 1, True))
-		self.assertEqual(cal.index[cal.enddate], (3388, 4748, False))
-		cal = CalendarSpec('Test', enddate='2013-01-05')
-		self.assertEqual(cal.enddate.isoformat(), '2013-01-05')
-		self.assertEqual(cal.index[cal.enddate], (3132, 4388, True))
-		cal = CalendarSpec('Test', startdate='2000-01-01')
-		self.assertEqual(cal.startdate.isoformat(), '2000-01-01')
-		self.assertEqual(cal.index[cal.startdate], (1, 1, True))
-		
-	def test_CalendarSpec_big_calendar_load_and_bizdays(self):
-		'loading a big calendar and computing bizdays between 2 dates'
-		cal = CalendarSpec('ANBIMA')
-		days = cal.bizdays(('2002-01-01', '2002-01-02'))
-		self.assertEqual(0, days, 'Wrong business days amount')
-		self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-31')), 21)
-		self.assertEqual(cal.bizdays(('2013-01-01', '2014-01-01')), 252)
-		self.assertEqual(cal.bizdays(('2014-01-01', '2015-01-01')), 252)
-		self.assertEqual(cal.bizdays(('2013-08-21', '2013-08-24')), 2)
-		self.assertEqual(cal.bizdays(('2002-07-12', '2002-07-22')), 6)
-		self.assertEqual(cal.bizdays(('2012-12-31', '2013-01-03')), 2)
-	
-	def test_CalendarSpec_currentdays(self):
-		'calendar count of currentdays'
-		cal = CalendarSpec('Test')
-		days = cal.currentdays(('2002-01-01', '2002-01-02'))
-		self.assertEqual(1, days, 'Wrong current days amount')
-	
-	def test_CalendarSpec_isbizday(self):
-		'calendar count of currentdays'
-		cal = CalendarSpec('Test')
-		self.assertEqual(cal.isbizday('2002-01-01'), False) # New year
-		self.assertEqual(cal.isbizday('2002-01-02'), True)  # First bizday
-		self.assertEqual(cal.isbizday('2002-01-05'), False) # Saturday
-	
-	def test_CalendarSpec_next_bizday(self):
-		"""next_bizday calculations"""
-		cal = CalendarSpec('Test')
-		self.assertEqual(cal.adjust_next('2001-01-01'), '2001-01-02')
-		self.assertEqual(cal.adjust_next('2001-01-02'), '2001-01-02')
-		
-	def test_CalendarSpec_previous_bizday(self):
-		"""previous_bizday calculations"""
-		cal = CalendarSpec('Test')
-		self.assertEqual(cal.adjust_previous('2001-08-12'), '2001-08-10')
-	
-	def test_CalendarSpec_seq(self):
-		'''sequence generator of bizdays'''
-		cal = CalendarSpec('Test')
-		seq = cal.seq(('2013-01-01', '2013-01-05'))
-		dts = ('2013-01-02', '2013-01-03', '2013-01-04')
-		for i, dt in enumerate(seq):
-			self.assertEqual(dt, dts[i])
-		seq = cal.seq(('2013-01-02', '2013-01-02'))
-		for i, dt in enumerate(seq):
-			self.assertEqual(dt, '2013-01-02')
-		seq = cal.seq(('2013-01-01', '2013-01-01'))
-		with self.assertRaises(StopIteration):
-			seq.next()
-			
-	def test_CalendarSpec_offset(self):
-		"""it should offset the given date by n days (forward or backward)"""
-		cal = CalendarSpec('Test')
-		self.assertEqual(cal.offset('2013-01-02', 1), '2013-01-03')
-		self.assertEqual(cal.offset('2013-01-02', 3), '2013-01-07')
-		self.assertEqual(cal.offset('2013-01-01', 1), '2013-01-03')
-		self.assertEqual(cal.offset('2013-01-01', 0), '2013-01-02')
-		self.assertEqual(cal.offset('2013-01-02', 0), '2013-01-02')
-		self.assertEqual(cal.offset('2013-01-02', -1), '2012-12-31')
-		self.assertEqual(cal.offset('2013-01-02', -3), '2012-12-27')
-		self.assertEqual(cal.offset('2013-01-01', -1), '2012-12-28')
+    def testCalendar_instanciate(self):
+        'it should instanciate the calendar'
+        cal = Calendar(startdate='2013-01-01', enddate='2013-12-31', weekdays=('Saturday', 'Sunday'))
+        self.assertEqual(cal.startdate.isoformat(), '2013-01-01')
+        self.assertEqual(cal.enddate.isoformat(), '2013-12-31')
+        self.assertEqual(cal.weekdays, ('Saturday', 'Sunday'))
+    
+    def testCalendar_bizdays(self):
+        'it should return the amount of business days'
+        cal = Calendar(startdate='2013-01-01', enddate='2013-12-31')
+        self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-02')), 1)
+        self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-05')), 3)
+        self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-06')), 3)
+        self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-07')), 4)
+    
+    def testCalendar_consistency_check(self):
+        'it should compare bizdays and currentdays'
+        cal = Calendar(startdate='2013-01-01', enddate='2013-12-31', weekdays=[])
+        self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-06')), cal.currentdays(('2013-01-01', '2013-01-06')))
+    
+    def testCalendarSpec_name(self):
+        "it should check the calendar's name"
+        cal = CalendarSpec('Test')
+        self.assertEqual(cal.name, 'Test')
+    
+    def testCalendarSpec(self):
+        'calendar instanciation'
+        with self.assertRaises(Exception):
+            CalendarSpec('AAA')
+        with self.assertRaises(Exception):
+            CalendarSpec('calAAA')
+        cal = CalendarSpec('Test')
+        self.assertEqual(cal, CalendarSpec('Test'))
+        self.assertEqual(cal.startdate.isoformat(), '2001-01-01')
+        self.assertEqual(cal.enddate.isoformat(), '2013-12-31')
+        self.assertEqual(len(cal.holidays), 4)
+        self.assertEqual(date(2001, 1, 1) in cal.holidays, True)
+        self.assertEqual(cal.index[cal.startdate], (1, 1, True))
+        self.assertEqual(cal.index[cal.enddate], (3388, 4748, False))
+        cal = CalendarSpec('Test', enddate='2013-01-05')
+        self.assertEqual(cal.enddate.isoformat(), '2013-01-05')
+        self.assertEqual(cal.index[cal.enddate], (3132, 4388, True))
+        cal = CalendarSpec('Test', startdate='2000-01-01')
+        self.assertEqual(cal.startdate.isoformat(), '2000-01-01')
+        self.assertEqual(cal.index[cal.startdate], (1, 1, True))
+        
+    def test_CalendarSpec_big_calendar_load_and_bizdays(self):
+        'loading a big calendar and computing bizdays between 2 dates'
+        cal = CalendarSpec('ANBIMA')
+        days = cal.bizdays(('2002-01-01', '2002-01-02'))
+        self.assertEqual(0, days, 'Wrong business days amount')
+        self.assertEqual(cal.bizdays(('2013-01-01', '2013-01-31')), 21)
+        self.assertEqual(cal.bizdays(('2013-01-01', '2014-01-01')), 252)
+        self.assertEqual(cal.bizdays(('2014-01-01', '2015-01-01')), 252)
+        self.assertEqual(cal.bizdays(('2013-08-21', '2013-08-24')), 2)
+        self.assertEqual(cal.bizdays(('2002-07-12', '2002-07-22')), 6)
+        self.assertEqual(cal.bizdays(('2012-12-31', '2013-01-03')), 2)
+    
+    def test_CalendarSpec_currentdays(self):
+        'calendar count of currentdays'
+        cal = CalendarSpec('Test')
+        days = cal.currentdays(('2002-01-01', '2002-01-02'))
+        self.assertEqual(1, days, 'Wrong current days amount')
+    
+    def test_CalendarSpec_isbizday(self):
+        'calendar count of currentdays'
+        cal = CalendarSpec('Test')
+        self.assertEqual(cal.isbizday('2002-01-01'), False) # New year
+        self.assertEqual(cal.isbizday('2002-01-02'), True)  # First bizday
+        self.assertEqual(cal.isbizday('2002-01-05'), False) # Saturday
+    
+    def test_CalendarSpec_next_bizday(self):
+        """next_bizday calculations"""
+        cal = CalendarSpec('Test')
+        self.assertEqual(cal.adjust_next('2001-01-01'), '2001-01-02')
+        self.assertEqual(cal.adjust_next('2001-01-02'), '2001-01-02')
+        
+    def test_CalendarSpec_previous_bizday(self):
+        """previous_bizday calculations"""
+        cal = CalendarSpec('Test')
+        self.assertEqual(cal.adjust_previous('2001-08-12'), '2001-08-10')
+    
+    def test_CalendarSpec_seq(self):
+        '''sequence generator of bizdays'''
+        cal = CalendarSpec('Test')
+        seq = cal.seq(('2013-01-01', '2013-01-05'))
+        dts = ('2013-01-02', '2013-01-03', '2013-01-04')
+        for i, dt in enumerate(seq):
+            self.assertEqual(dt, dts[i])
+        seq = cal.seq(('2013-01-02', '2013-01-02'))
+        for i, dt in enumerate(seq):
+            self.assertEqual(dt, '2013-01-02')
+        seq = cal.seq(('2013-01-01', '2013-01-01'))
+        with self.assertRaises(StopIteration):
+            seq.next()
+            
+    def test_CalendarSpec_offset(self):
+        """it should offset the given date by n days (forward or backward)"""
+        cal = CalendarSpec('Test')
+        self.assertEqual(cal.offset('2013-01-02', 1), '2013-01-03')
+        self.assertEqual(cal.offset('2013-01-02', 3), '2013-01-07')
+        self.assertEqual(cal.offset('2013-01-01', 1), '2013-01-03')
+        self.assertEqual(cal.offset('2013-01-01', 0), '2013-01-02')
+        self.assertEqual(cal.offset('2013-01-02', 0), '2013-01-02')
+        self.assertEqual(cal.offset('2013-01-02', -1), '2012-12-31')
+        self.assertEqual(cal.offset('2013-01-02', -3), '2012-12-27')
+        self.assertEqual(cal.offset('2013-01-01', -1), '2012-12-28')
 
 
 if __name__ == '__main__':
-	unittest.main(verbosity=1)
+    unittest.main(verbosity=1)
